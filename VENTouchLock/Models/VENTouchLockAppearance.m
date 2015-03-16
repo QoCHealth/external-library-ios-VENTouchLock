2575ddad37d299a669b8debdd6c9b6833ae892fe
normKei <kei@normative.com>
2015-03-16 09:52:51 -0400

    Update VENTouchLockAppearance.m
    forces match with local file

--- VENTouchLock/Models/VENTouchLockAppearance.m
+++ VENTouchLock/Models/VENTouchLockAppearance.m
@@ -9,17 +9,15 @@
         _passcodeViewControllerTitleColor = [UIColor blackColor];
         _passcodeViewControllerCharacterColor = [UIColor blackColor];
         _passcodeViewControllerBackgroundColor = [UIColor colorWithRed:239/255.0f green:239/255.0f blue:244/255.0f alpha:1.0f];
-        _passcodeViewControllerShouldEmbedInNavigationController = NO;
         _cancelBarButtonItemTitle = NSLocalizedString(@"Cancel", nil);
-        _createPasscodeInitialLabelText = NSLocalizedString(@"Enter a new passcode", nil);
-        _createPasscodeConfirmLabelText = NSLocalizedString(@"Please re-enter your passcode", nil);
+        _createPasscodeInitialLabelText = NSLocalizedString(@"Enter a new password", nil);
+        _createPasscodeConfirmLabelText = NSLocalizedString(@"Please re-enter your password", nil);
         _createPasscodeMismatchedLabelText = NSLocalizedString(@"Passwords did not match. Try again", nil);
-        _createPasscodeViewControllerTitle = NSLocalizedString(@"Set Passcode", nil);
-        _enterPasscodeInitialLabelText = NSLocalizedString(@"Enter your passcode", nil);
-        _enterPasscodeIncorrectLabelText = NSLocalizedString(@"Incorrect passcode. Try again.", nil);
-        _enterPasscodeViewControllerTitle = NSLocalizedString(@"Enter Passcode", nil);
+        _createPasscodeViewControllerTitle = NSLocalizedString(@"Set Password", nil);
+        _enterPasscodeInitialLabelText = NSLocalizedString(@"Enter your password", nil);
+        _enterPasscodeIncorrectLabelText = NSLocalizedString(@"Incorrect password. Try again.", nil);
+        _enterPasscodeViewControllerTitle = NSLocalizedString(@"Enter Password", nil);
         _splashShouldEmbedInNavigationController = NO;
-        _touchIDCancelPresentsPasscodeViewController = NO;
     }
     return self;
 }
